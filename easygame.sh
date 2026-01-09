c8beaf125de76027d5b53a3e36c365e5e6eb1fe0
Chinmay Joshi <chinmay30joshi@gmail.com>
2026-01-09 14:07:28 +0530

    Create easygame.sh

---
+++ easygame.sh
@@ -0,0 +1,16 @@
+# Creator Chinu Bhai
+
+# Google Cloud Networking Lab Setup
+# Expertly crafted by Easy Game
+
+Use the code to complete the lab quickly without any issue and manual hard work.
+
+'''
+
+curl -LO https://raw.githubusercontent.com/Chinmayjoshi2005/EasyGame/refs/heads/main/easygame.sh
+sudo chmod +x easygame.sh
+./easygame.sh
+
+'''
+
+Congrulations Your Lab is completed Successfully after using this link wait for 3 to 5 minutes it will be completed
